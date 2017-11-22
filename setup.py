4e2acfcd2cd384956dacc2c415dd38e17f0e162b
Allan Callaghan <allan.callaghan@global.com>
2017-11-22 11:01:11 +0000

    setup.py was not functional .. experimenting

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
-from setuptools import setup
+from distutils.core import setup
 from os import path
 
 _here = path.dirname(path.abspath(__file__))
-_readme = open(path.join(_here, 'README.md')).read()
 
 setup(
     name='pytest-appium',
-    description='',
-    long_description=_readme,
+    description='PyTest bindings for Appium',
     version='0.0.1',
-    url='http://git.int.thisisglobal.com/interactive/pytest-appium',
-    author='Global',
-    author_email=' leicestersquare-interactive-developers@global.com',
-    license='MIT',
-    platform='any',
-    py_modules=['pytest_appium'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
-    keywords='appium pytest'
+    #py_modules=['pytest_appium'],
+    packages=['pytest_appium'],
+    long_description=open(path.join(_here, 'README.md')).read(),
+
+    #url='http://git.int.thisisglobal.com/interactive/pytest-appium',
+    #author='Global',
+    #author_email=' leicestersquare-interactive-developers@global.com',
+    #license='MIT',
+    #platform='any',
+    #classifiers=[
+    #    'Development Status :: 3 - Alpha',
+    #    'Programming Language :: Python',
+    #    'Programming Language :: Python :: 3',
+    #    'Programming Language :: Python :: 3.5',
+    #    'Intended Audience :: Developers',
+    #    'License :: OSI Approved :: MIT License',
+    #    'Topic :: Software Development :: Libraries',
+    #    'Topic :: Software Development :: Libraries :: Python Modules'
+    #],
+    #keywords='appium pytest'
 )
